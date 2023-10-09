0f47e53f21014555f8593e7b3e2ad1c333849c50
SectorMind <66206867+SectorMind@users.noreply.github.com>
2023-10-09 18:35:30 +0300

    Add files via upload

---
+++ lesson 2/80_js_loop/main.js
@@ -0,0 +1,16 @@
+// Loop
+// let i = 0
+//
+// while(i < 10) {
+//     i ++
+//     console.log(i)
+// }
+//
+// do {
+//     i ++
+//     console.log(i)
+// } while (i < 5)
+
+for (let i = 1; i <= 10; i = i + 2) {
+    console.log(`Пройден ${i} круг`)
+}
