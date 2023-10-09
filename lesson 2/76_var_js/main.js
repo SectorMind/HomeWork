e68dff67e282bdb5fbacd09b38c6958d498cce13
SectorMind <66206867+SectorMind@users.noreply.github.com>
2023-10-09 17:10:17 +0300

    Add files via upload

---
+++ lesson 2/76_var_js/main.js
@@ -0,0 +1,59 @@
+// const firstName = 'John'
+// let age = 20
+// const lastName = 'Smith'
+
+// firstName = 'Alex'
+
+// var lastName = 'Smith'
+
+//console.log(firstName, lastName, age, 'y.o')
+// console.log(lastName)
+
+
+// const a = 5
+
+// const sum = 6 + a
+
+// console.log(sum)
+
+//console.log('exponentiation', 5**2)
+//console.log('remainder', 5%2)
+
+// console.log('Hello, ' + 'world!')
+
+// const num = 2
+// console.log(-num)
+
+// 1) string
+
+const tool = 'Инструмент'
+const box = 'ящик'
+
+console.log(`${tool} лежит в ${box}е`)
+
+// 2) number
+
+
+
+// 3) boolean
+
+const user = 'User'
+const age = 25
+const isAdmin = true
+console.log(`${user} admin: ${isAdmin}`)
+console.log(typeof(isAdmin))
+
+// 4) undefined
+
+let foo
+console.log('foo', foo)
+
+// 5) null
+
+let sum = null
+console.log('sum', null)
+
+// 6) bigint*
+// 7) symbol*
+
+
