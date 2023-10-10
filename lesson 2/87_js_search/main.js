fdeec5c7a3085210ef5319e66044a34ffd405982
SectorMind <66206867+SectorMind@users.noreply.github.com>
2023-10-10 17:49:16 +0300

    Add files via upload

---
+++ lesson 2/87_js_search/main.js
@@ -0,0 +1,17 @@
+// const element = document.querySelector('.block')
+
+// element.textContent = 'text'
+// element.id = '123'
+// element.style.color = 'red'
+// element.style.fontSize = '30px'
+
+// console.log(element)
+
+// const element = document.querySelector('div')
+const elements = document.querySelectorAll('div')
+// console.log(elements[1])
+
+elements.forEach((element) => {
+    // console.log(element)
+    element.style.color = '#F511DD'
+})
