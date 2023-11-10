af17bd3050ce9887f7ee4a63dd4631cc845c9fb0
SectorMind <66206867+SectorMind@users.noreply.github.com>
2023-11-10 16:39:36 +0000

    Add files via upload

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,6 @@
 (function() {
 
+    /* burger menu */
     document.addEventListener('click', burgerInit);
 
     function burgerInit(e) {
@@ -47,4 +48,35 @@
             document.body.classList.remove('body--opened-modal');
         }
     });
+
+    /* tabs */
+
+    const tabControls = document.querySelector('.tab-controls');
+
+    tabControls.addEventListener('click', toggleTab);
+
+    function toggleTab(e) {
+        const tabControl = e.target.closest('.tab-controls__link');
+
+        if (!tabControl) return;
+        e.preventDefault();
+        if (tabControl.classList.contains('tab-controls__link--active')) return;
+
+        const tabContentID = tabControl.getAttribute('href');
+        const tabContent = document.querySelector(tabContentID);
+        const activeControl = document.querySelector('.tab-controls__link--active');
+        const activeContent = document.querySelector('.tab-content--show');
+        
+        if (activeControl) {
+            activeControl.classList.remove('tab-controls__link--active');
+        }
+        if (activeContent) {
+            activeContent.classList.remove('tab-content--show');
+        }
+        
+        tabControl.classList.add('tab-controls__link--active');
+        tabContent.classList.add('tab-content--show');
+        
+        
+    }
 })();
