508b96c8ae5985007d9bbb01ededab6b8c669341
SectorMind <66206867+SectorMind@users.noreply.github.com>
2023-11-26 17:45:54 +0000

    Add files via upload

--- docs/js/main.js
+++ docs/js/main.js
@@ -77,4 +77,28 @@
         tabControl.classList.add('tab-controls__link--active');
         tabContent.classList.add('tab-content--show');
     }
+
+    /* accordion */
+
+    const accordionLists = document.querySelectorAll('.accordion-list');
+
+    accordionLists.forEach(el =>{
+
+        el.addEventListener('click', (e) => {
+
+            const accordionControl = e.target.closest('.accordion-list__control');
+            if (!accordionControl) return;
+            const accordionItem = accordionControl.parentElement;
+            const accordionContent = accordionControl.nextElementSibling;
+
+            accordionItem.classList.toggle('accordion-list__item--opened');
+
+            if (accordionItem.classList.contains('accordion-list__item--opened')) {
+                accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+            } else {
+                accordionContent.style.maxHeight = null;
+            }
+        });
+    })
+
 })();
